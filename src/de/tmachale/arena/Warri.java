6aaa848a7596095f55e400003cf15c8058adfade
tmachale <hallunke@googlemail.com>
2015-09-24 21:52:20 +0200

    #2 create Basic Warri Class

---
+++ src/de/tmachale/arena/Warri.java
@@ -0,0 +1,26 @@
+package de.tmachale.arena;
+
+public class Warri {
+
+	private int life, attack, agi;
+	public int getLife(){
+		return life;
+	}
+	public void setLife(int life) {
+		this.life = life;
+	}
+	public int getAttack() {
+		return attack;
+	}
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+	public int getAgi() {
+		return agi;
+	}
+	public void setAgi(int agi) {
+		this.agi = agi;
+	}
+
+	
+}
